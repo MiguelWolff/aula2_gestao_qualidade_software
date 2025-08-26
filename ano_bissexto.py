3cfc3ee339e34cb8478ee18cede181c77b19bd3b
MiguelWolff <149209042+MiguelWolff@users.noreply.github.com>
2025-08-25 21:04:48 -0300

    Adicionado validador de ano

--- ano_bissexto.py
+++ ano_bissexto.py
@@ -4,6 +4,12 @@ def contar_bissextos(limite):
     multiplos_400 = limite // 400 - (1999 // 400)
     return multiplos_4 - multiplos_100 + multiplos_400
 
-# Programa principal
-ano_final = int(input("Digite o ano final: "))
+# Programa principal com validador
+while True:
+    ano_final = int(input("Digite o ano final (entre 2000 e 4000): "))
+    if 2000 <= ano_final <= 4000:
+        break
+    else:
+        print("Ano inválido! Digite um valor entre 2000 e 4000.")
+
 print(f"Quantidade de anos bissextos de 2000 até {ano_final}: {contar_bissextos(ano_final)}")
